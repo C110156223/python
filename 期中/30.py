f91b40f3654d847930c1cf03c88bae8c2dbc95ee
C110156223 <102135417+C110156223@users.noreply.github.com>
2022-06-25 17:27:43 +0800

    Update 30.py

--- 期中/30.py
+++ 期中/30.py
@@ -1,13 +1,5 @@
-'''
-que=0
-for i in range(10):
-    if que>0:
-        que=int(input("猜數字(4位數):"))
-    elif que=='0000':
-        break  
-'''    
-que=int(input("猜數字(4位數):")) 
-ans='1234'
+que=list(input("猜數字(4位數):")) 
+ans=[1,2,3,4]
 A=0
 B=0
 for i in range(4):
@@ -16,24 +8,3 @@ for i in range(4):
     elif ans[i] in que:
         B+=1
 print(A,"A",B,"B")
-
-# right = ['1','2',"3","4"]
-
-# while True:
-#     a=0
-#     b=0
-#     user =list(input('猜四位數字:'))
-#     if user.count('0')==4:
-#         print('結束')
-#         break
-#     if len(user)==4:
-#         for i in range(4):
-#             if (right[i]==user[i]):
-#                 a+=1
-#             elif(right.count(user[i])==1 and right[i]!=user[i]):
-#                 b+=1
-#         print(str(a)+'A',str(b)+"B")
-#     elif len(user)!=4:
-#         print('輸入錯誤')
-#     if a==4:
-#         break
