cf993d29e966b1f2b62d2fd3c76b6916f55e83e5
C110156223 <102135417+C110156223@users.noreply.github.com>
2022-06-25 16:58:15 +0800

    Add files via upload

---
+++ 期中/21.py
@@ -0,0 +1,13 @@
+id=input("輸入欲查詢學號為:")
+name=['Tom','Cat','Nana','Lim','Won']
+d=['DTGD','CSIE','ASIE','DBA','FDD']
+if id=='123':
+    print("學生資料為:",id,name[0],d[0])
+elif id=='456':
+    print("學生資料為:",id,name[1],d[1])
+elif id=='789':
+    print("學生資料為:",id,name[2],d[2])
+elif id=='321':
+    print("學生資料為:",id,name[3],d[3])
+elif id=='654':
+    print("學生資料為:",id,name[4],d[4])
